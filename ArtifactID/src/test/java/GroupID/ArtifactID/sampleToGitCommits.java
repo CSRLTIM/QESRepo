64508e78d11b21b9789322cb2d137c45de06f2fa
10718765 <10718765@NANXCV00F718405>
2023-10-26 18:19:23 +0530

    committing to rebase

--- ArtifactID/src/test/java/GroupID/ArtifactID/sampleToGitCommits.java
+++ ArtifactID/src/test/java/GroupID/ArtifactID/sampleToGitCommits.java
@@ -5,7 +5,8 @@ import org.testng.annotations.Test;
 public class sampleToGitCommits {
 	
 	@Test
-public void sampletest() {
+public void sampletest() 
+{
 	System.out.println("test");
 	System.out.println("commiting from collabrator mcsekhara");
 	System.out.println("Direct update in GIT");
