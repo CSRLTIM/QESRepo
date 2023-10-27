752ec1533edded8c6ff9ddba51f379c3eb578770
10718765 <10718765@NANXCV00F718405>
2023-10-27 17:29:18 +0530

    commit to mcsekharamain branch

--- ArtifactID/src/test/java/GroupID/ArtifactID/sampleToGitCommits.java
+++ ArtifactID/src/test/java/GroupID/ArtifactID/sampleToGitCommits.java
@@ -12,6 +12,7 @@ public void sampletest()
 	System.out.println("Direct update in GIT");
 	System.out.println("CSRLTIM Committing");
 	//updated
+	System.out.println("MCSekhara Changes Committing");
 	
 }
 
